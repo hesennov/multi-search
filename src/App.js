366ed3727a2651d845a2c08e52d2ef41e6544cf2
hesennov <hasanhasanov345@gmail.com>
2023-03-24 16:49:07 +0400

    multi search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Users } from "./users";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Table from "./Table";
+import axios from "axios";
 
 function App() {
+  const [query, setquery] = useState("");
+  // console.log(query);
+  let [users, setusers] = useState([]);
+  useEffect(() => {
+    async function getResults() {
+      const results = await axios("https://jsonplaceholder.typicode.com/users");
+      setusers(results.data);
+    }
+    getResults();
+  }, []);
+  console.log(users);
+  const keys = ["name", "username", "email"];
+  const search = (data) => {
+    return data.filter(
+      (user) => keys.some((key) => user[key].toLowerCase().includes(query))
+      // user.name.toLowerCase().includes(query) ||
+      // user.username.toLowerCase().includes(query) ||
+      // user.email.toLowerCase().includes(query)
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+      <h1>react</h1>
+      <input
+        type="text"
+        placeholder="search..."
+        className="search"
+        onChange={(e) => setquery(e.target.value)}
+      />
+      <Table data={search(users)} />
     </div>
   );
 }
